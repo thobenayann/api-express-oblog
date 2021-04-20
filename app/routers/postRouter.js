0a8a7221dee465fd8b8a7b9e3b06b629d355d64e
thobenayann <63640040+thobenayann@users.noreply.github.com>
2021-04-20 23:38:08 +0200

    add regex

--- app/routers/postRouter.js
+++ app/routers/postRouter.js
@@ -4,7 +4,7 @@ const postsController = require('../controllers/postsController');
 const router = express.Router();
 
 router.get('/', postsController.allPosts);
-router.get('/:postId', postsController.postById);
-router.get('/category/:categoryId', postsController.postsByCategoryId);
+router.get('/:postId(\\d+)', postsController.postById);
+router.get('/category/:categoryId(\\d+)', postsController.postsByCategoryId);
 
 module.exports = router;
